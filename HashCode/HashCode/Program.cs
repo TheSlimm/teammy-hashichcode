3358ad60e324c0a5000cc2c33c578ab93a70603f
Sabri Labassi <sabri.labassi@netapsys.fr>
2017-02-23 17:27:53 +0100

    init

---
+++ HashCode/HashCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HashCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wazaaaa");
+            Console.WriteLine("YOLO");
+            Console.Read();
+        }
+    }
+}
