1e715fa5d4a4a95c1992e7cb34cae6868aa43d09
Sabri Labassi <sabri.labassi@netapsys.fr>
2017-02-23 19:17:06 +0100

    ergezrg

--- HashCode/HashCode/CacheServer.cs
+++ HashCode/HashCode/CacheServer.cs
@@ -9,5 +9,8 @@ namespace HashCode
     public class CacheServer
     {
         public int Id { get; set; }
+        public int Size { get; set; }
+        public List<Video> Videos { get; set; }
+
     }
 }
